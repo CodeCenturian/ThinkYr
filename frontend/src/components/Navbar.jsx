bbe29d93f19385e641d68d204c05f377f44ec71f
CodeCenturian <ashutoshkumariiitb@gmail.com>
2025-07-06 20:48:44 +0530

    updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,11 @@ const Navbar = () => {
     <header className="bg-base-300 border-b border-base-content/10">
       <div className="mx-auto max-w-6xl p-4">
         <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">ThinkYr</h1>
+          <img
+              src="/Thinkyrname.png"          // ← public/Thinkyrname.png → accessible at “/Thinkyrname.png”
+              alt="ThinkYr Logo"
+              className="h-10 w-22 object-contain"
+            />
           <div className="flex items-center gap-4">
             <Link to={"/create"} className="btn btn-primary">
               <PlusIcon className="size-5" />
